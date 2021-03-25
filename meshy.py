fc5fea69ef0f7183a5be6b40e3d77d81ab5f9bab
Sean Osborn Wiik <sean0262@gmail.com>
2021-03-25 14:00:15 +0100

    Update meshy.py

--- meshy.py
+++ meshy.py
@@ -1,7 +1,7 @@
 import meshtastic
 from pubsub import pub
 
-# When a package arrives, get 'from' and 'decoded'.'data'.'text. This is for getting the message and the ID from the package
+# When a package arrives, get 'from' and 'decoded'.'data'.'text'. This is for getting the message and the ID from the package
 def onText(packet, interface): # called when a packet arrives
     print(f"\n\n{packet.get('from')}: {packet.get('decoded').get('data').get('text')}\n")
 
@@ -22,7 +22,7 @@ def sendMessage(path):
        message = input()
        interface.sendText(message)
 
-# Choice for selecting path and then go into sendMessage fnuction.
+# Choice for selecting path and then go into sendMessage function.
 def main():
     print("Welcome to Meshy!\n")
     ch = input("Do you wish to specify where the USB is? (/dev/ttyUSB0 - Example for Linux)\nY/n ")
